f3e1d9cd067fc63846946bb5daeaeb9f5627b9a6
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-10-16 15:27:37 +0330

    feat: add templates

---
+++ bin/bash/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./bin/template/dns/__init__.py ./dns/__init__.py
+cp ./bin/template/mail/__init__.py ./mail/__init__.py
+cp ./bin/template/paas/__init__.py ./paas/__init__.py
+cp ./bin/template/dbaas/__init__.py ./dbaas/__init__.py
+cp ./bin/template/object_storage/__init__.py ./object_storage/__init__.py
---
+++ bin/cmd/init.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+copy .\bin\template\dns\__init__.py .\dns\__init__.py
+copy .\bin\template\mail\__init__.py .\mail\__init__.py
+copy .\bin\template\paas\__init__.py .\paas\__init__.py
+copy .\bin\template\dbaas\__init__.py .\dbaas\__init__.py
+copy .\bin\template\object_storage\__init__.py .\object_storage\__init__.py
