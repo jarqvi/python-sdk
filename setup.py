1fc7f90b649023f3ea4c1e4c80320a3c3aabf6c9
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-10-17 11:32:51 +0330

    refactor: update builder sdk

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
-NAME = "openapi-client"
+NAME = "jarqvi-openapi-client"
 VERSION = "1.0.0"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
@@ -48,4 +48,5 @@ setup(
     Manage your services using our API 
     """,  # noqa: E501
     package_data={"openapi_client": ["py.typed"]},
+    setup_requires=['wheel']
 )
